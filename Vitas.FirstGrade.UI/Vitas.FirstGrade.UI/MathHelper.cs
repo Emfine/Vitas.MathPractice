492322be47038e27b21f376f5c28bbe963f6e3ec
Emfine Zheng <zyf601563839@live.cn>
2022-01-17 15:45:07 +0800

    1.100道题； 2.输出文件按日期； 3.3个数字的混合加减运算； 4.没有小数、没有负数；

---
+++ Vitas.FirstGrade.UI/Vitas.FirstGrade.UI/MathHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitas.FirstGrade.UI
+{
+    internal class MathHelper
+    {
+        private readonly Random rad;
+        private readonly List<string> operList;
+
+        internal MathHelper()
+        {
+            rad = new Random();
+            operList = new List<string>() { "+", "-" };
+        }
+
+        internal int CalcByDataTable(string expression)
+        {
+            object result = new DataTable().Compute(expression, "");
+            return Convert.ToInt32(result);
+        }
+
+        internal StringBuilder TwoDigitMix(int num)
+        {
+            int minusMax = 100;
+            var operQueue = new List<string>();
+            var expression = new StringBuilder();
+            expression.Append(rad.Next(10, minusMax));
+
+            for (int idx = 0; idx < num - 1; idx++)
+            {
+                var oList = operList.Where(x => !operQueue.Contains(x)).ToList();
+                int opIdx = rad.Next(0, oList.Count);
+                operQueue.Add(oList[opIdx]);
+
+                minusMax = 99;
+                //如果是减法需要保证减数小于被减数
+                if (operQueue[idx] == "-")
+                {
+                    //计算出减数最大值，如果超出99则最大值按99
+                    int max = CalcByDataTable(expression.ToString());
+                    if (max < 100) minusMax = max;
+                }
+                expression.Append($"{operQueue[idx]}{rad.Next(10, minusMax)}");
+            }
+
+            expression.Append("=");
+
+            return expression;
+        }
+    }
+}
---
+++ Vitas.FirstGrade.UI/Vitas.FirstGrade.UI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Vitas.FirstGrade.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var helper = new MathHelper();
+            DateTime now = DateTime.Now;
+            int row = 100;
+            int num = 3;
+            int column = 4;
+            using (var sw = new StreamWriter($"{now.ToString("yyyy-MM-dd")}.txt"))
+            {
+                Console.SetOut(sw);
+
+                for (int ridx = 0; ridx < row / column; ridx++)
+                {
+                    for (int cidx = 0; cidx < column; cidx++)
+                    {
+                        var cell = new StringBuilder();
+                        string sort = $"{cidx * row / column + ridx + 1}. ";
+                        cell.Append(sort.PadLeft(5));
+                        var expression = helper.TwoDigitMix(num);
+                        cell.Append(expression);
+                        Console.Write(cell.ToString().PadRight(cell.Length + 6));
+                    }
+                    Console.WriteLine();
+                }
+
+                sw.Flush();
+            }
+            Console.ReadLine();
+        }
+    }
+}
